e84585f3db2b9dc2bb52048e8505375a23b7a0d9
Natalie <mynatalie05@icloud.com>
2026-03-16 21:42:34 -0700

    completed forward selection

--- main.py
+++ main.py
@@ -51,6 +51,7 @@ def predict_one_nearest_neighbor(classes, features, index, selected_features):
     
     return classes[nearest_index]
 
+# hide an instance, predict its class, and check if it was correct
 def leave_one_out_cross_validation(classes, features, selected_features):
     correct = 0
 
@@ -63,6 +64,52 @@ def leave_one_out_cross_validation(classes, features, selected_features):
     accuracy = correct / len(features)
     return accuracy
 
+# adding 1 to the features for display since my features start at 0
+def format_feature_set(selected_features):
+    display_numbers = []
+
+    for feature in selected_features:
+        display_numbers.append(str(feature + 1))
+
+    return "{" + ",".join(display_numbers) + "}"
+
+# find good subset by building up from none
+def forward_selection(classes, features):
+    current_set = []
+    best_accuracy = 0
+    best_set = []
+    num_features = len(features[0])
+
+    print("\nBeginning search.\n")
+
+    for level in range(num_features):
+        feature_to_add_at_this_level = None
+        best_accuracy_at_this_level = 0
+
+        # dont add the same feature twice
+        for feature in range(num_features):
+            if feature in current_set:
+                continue
+
+            candidate = current_set + [feature]
+            accuracy = leave_one_out_cross_validation(classes, features, candidate)
+
+            print(f"Using feature(s) {format_feature_set(candidate)} accuracy is {accuracy:.4f}")
+
+            if accuracy > best_accuracy_at_this_level:
+                best_accuracy_at_this_level = accuracy
+                feature_to_add_at_this_level = feature
+        
+        current_set.append(feature_to_add_at_this_level)
+
+        print(f"Feature set {format_feature_set(current_set)} was best, accuracy is {best_accuracy_at_this_level:.4f}\n")
+
+        if best_accuracy_at_this_level > best_accuracy:
+            best_accuracy = best_accuracy_at_this_level
+            best_set = current_set.copy()
+
+    print(f"Finished search!! The best feature subset is {format_feature_set(best_set)}, "
+        f"which has an accuracy of {best_accuracy:.4f}")
 
 def main():
     print("Welcome to Natalie's Feature Selection Algorithm.")
@@ -76,20 +123,15 @@ def main():
 
     print(f"\nThis dataset has {num_features} features (not including the class attribute), with {num_instances} instances.")
 
-# testing sanity check 1 (7, 10, 12) and 2 (10, 8, 2)
-    selected_features = [9, 7, 1]
+    all_features = list(range(num_features))
+    accuracy = leave_one_out_cross_validation(classes, features, all_features)
 
-    test_index = 0
-    predicted = predict_one_nearest_neighbor(classes, features, test_index, selected_features)
+    print(
+        f"Running nearest neighbor with all {num_features} features, using leaving-one-out evaluation, "
+        f"I get an accuracy of {accuracy:.4f}"
+    )
 
-#printing to make sure the nearest neighbor is working
-    print("\nNearest neighbor test:")
-    print(f"Actual class: {classes[test_index]}")
-    print(f"Predicted class: {predicted}")
-
-    accuracy = leave_one_out_cross_validation(classes, features, selected_features)
-    print(f"\nRunning nearest neighbor with all {num_features} features, using leaving-one-out evaluation, I get an accuracy of {accuracy:.4f}")
-
+    forward_selection(classes, features)
 
 if __name__ == "__main__":
     main()
